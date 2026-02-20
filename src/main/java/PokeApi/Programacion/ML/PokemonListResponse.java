c0b17f3353d4e876b9ff8fa1d655321da0e6b949
mariovilchis <mariovilchis01@hotmail.com>
2026-02-20 17:20:58 -0600

    Verificacion correo

--- src/main/java/PokeApi/Programacion/ML/PokemonListResponse.java
+++ src/main/java/PokeApi/Programacion/ML/PokemonListResponse.java
@@ -1,34 +1,65 @@
 package PokeApi.Programacion.ML;
 
-
-
 import java.util.List;
 
-
-
-
 public class PokemonListResponse {
+
     private int count;
     private String next;
     private String previous;
     private List<PokemonResumen> results;
 
-    public int getCount() { return count; }
-    public void setCount(int count) { this.count = count; }
-    public String getNext() { return next; }
-    public void setNext(String next) { this.next = next; }
-    public String getPrevious() { return previous; }
-    public void setPrevious(String previous) { this.previous = previous; }
-    public List<PokemonResumen> getResults() { return results; }
-    public void setResults(List<PokemonResumen> results) { this.results = results; }
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public List<PokemonResumen> getResults() {
+        return results;
+    }
+
+    public void setResults(List<PokemonResumen> results) {
+        this.results = results;
+    }
 }
 
 class PokemonResumen {
+
     private String name;
     private String url;
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
